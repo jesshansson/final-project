6eb72be88ad5e82430977a830fdd91439eac2106
Jessica Hansson <jessica.hansson@hotmail.se>
2023-01-16 11:24:38 +0100

    Updated schema, troubleshooting

--- backend/server.js
+++ backend/server.js
@@ -209,7 +209,7 @@ app.get("/profile/:id", authenticateUser)
 app.get("/profile/:id", async (req, res) => {
   const { id } = req.params
   try {
-    const singleUser = await User.findById(id)
+    const singleUser = await User.findById(id) //.populate("culture")
     res.status(200).json({
       success: true,
       response: singleUser
